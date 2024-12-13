277cf98f3c41c65e1984cf9409be3b9be5593bbc
Harihara Sudhan <nhsharsudhan2307@gmail.com>
2024-12-13 11:41:44 +0530

    Nlp

--- spacy basics.py
+++ spacy basics.py
@@ -1,4 +1,5 @@
 import spacy
+from spacy import displacy
 
 nlp = spacy.load("en_core_web_sm")
 
@@ -14,6 +15,10 @@ print("tokens:",tokens)
 lemmas = [(token.text, token.lemma_) for token in doc]
 print("Lemmas:", lemmas)
 
+with doc.retokenize() as retokenizer:
+    retokenizer.merge(doc[3:5], attrs={"LEMMA": "Sudhan"})
+print("After:", [token.text for token in doc])
+
 #entity
 entities = [(ent.text, ent.label_) for ent in doc.ents]
 print("Entities:", entities)
@@ -21,3 +26,9 @@ print("Entities:", entities)
 #stopWords
 stop_words = [token.text for token in doc if token.is_stop]
 print("stop Words:", stop_words)
+
+#sentence segmentation
+doc = nlp("Whats Up!. Nice to meet you.")
+#assert doc.has_annotation("SENT_START")
+for sent in doc.sents:
+    print(sent.text)
